52ed3edb1d38cd41a74ece7f5b05496857e5fe41
Ítalo Alves <italooko@gmail.com>
2022-08-26 17:38:26 -0300

    Adiciona main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+import serial
+
+from connection.uart import Uart
+
+class AirFryer:
+    port = "/dev/serial1"
+    baudrate = 9600
+    timeout = 2
+    matricula = [3, 6, 6, 6]
+
+    def __init__(self):
+        self.uart = Uart(self.port, self.baudrate, self.timeout)
+        self.inicia_servicos()
+
+    def verifica_botoes(self):
+        comando = b'\x01\x23\xc3'
+
+        while True:
+            uart.envia(comando, self.matricula)
+            time.sleep(0.2)
+            recebido = uart.recebe()
+            time.sleep(0.3)
+
+    def verifica_temperatura(self):
+        comando = b'\x01\x23\xc1'
+
+        while True:
+            uart.envia(comando, self.matricula)
+            time.sleep(0.2)
+            recebido = uart.recebe()
+            time.sleep(0.8)
+
+    def inicia_servicos(self):
+        thread_botoes = Thread(target=self.verifica_botoes, args=())
+        thread_botoes.start() 
+
+        thread_temperatura = Thread(target=self.verifica_temperatura, args=())
+        thread_temperatura.start()
+
+AirFryer()
